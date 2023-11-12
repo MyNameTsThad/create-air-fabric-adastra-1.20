2889f710fb76faa224bfb5046c4a46f9d67e89ce
bitheaven <bit.heaven@bk.ru>
2023-11-12 23:19:23 +0500

    Remove spaces

--- src/main/java/ru/bitheaven/createairfabric/mixin/RemainingAirOverlayMixin.java
+++ src/main/java/ru/bitheaven/createairfabric/mixin/RemainingAirOverlayMixin.java
@@ -1,15 +1,13 @@
 package ru.bitheaven.createairfabric.mixin;
 
-
 import com.simibubi.create.content.equipment.armor.RemainingAirOverlay;
+import net.minecraft.client.player.LocalPlayer;
 import net.minecraft.tags.TagKey;
 import net.minecraft.world.level.material.Fluid;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 import ru.bitheaven.createairfabric.CreateAirFabric;
-
-import net.minecraft.client.player.LocalPlayer;
 
 @Mixin(RemainingAirOverlay.class)
 public class RemainingAirOverlayMixin {
@@ -19,6 +17,6 @@ public class RemainingAirOverlayMixin {
     @Redirect(at = @At(value = "INVOKE", target = "Lnet/minecraft/client/player/LocalPlayer;isEyeInFluid(Lnet/minecraft/tags/TagKey;)Z"),
             method = "render(Lnet/minecraft/client/gui/GuiGraphics;II)V")
     private static boolean redirectRender(LocalPlayer player, TagKey<Fluid> fluidTagKey) {
-        return player.isEyeInFluid(fluidTagKey) || CreateAirFabric.airQualityActivatesHelmet(player);
+        return player.isEyeInFluid(fluidTagKey) || !CreateAirFabric.isOxygen(player);
     }
 }
