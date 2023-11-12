8862a20b6b460f3c6f829478945726c98b0eeff5
bitheaven <bit.heaven@bk.ru>
2023-11-12 23:19:12 +0500

    Remove spaces

--- src/main/java/ru/bitheaven/createairfabric/mixin/MinecraftMixin.java
+++ src/main/java/ru/bitheaven/createairfabric/mixin/MinecraftMixin.java
@@ -1,11 +1,8 @@
 package ru.bitheaven.createairfabric.mixin;
 
 import ru.bitheaven.createairfabric.CreateAirFabric;
-
 import net.minecraft.client.Minecraft;
-
 import net.minecraft.client.main.GameConfig;
-
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
